83870ec96b40d6a27b511dc243b349faf183b682
Iris <31053695+AlgoritmoAlgoritmo@users.noreply.github.com>
2025-04-08 21:27:56 -0300

    Worked some more on the main char clothes model

--- NSFW15/Assets/Scripts/Misc/BGMController.cs
+++ NSFW15/Assets/Scripts/Misc/BGMController.cs
@@ -13,74 +13,55 @@ using UnityEngine.Audio;
 
 
 namespace Jam15.Misc {
-        public class BGMController : MonoBehaviour {
-            #region  Variables
-            [SerializeField]
-            private AudioClip[] bgmTracks;
-            [SerializeField]
-            private AudioMixer audioMixer;
-            [SerializeField]
-            private bool autoStart;
-            [SerializeField]
-            private bool isCyclicModeOn;
-            [SerializeField]
-            private float waitingTimeToRefreshInSeconds = 1f;
+    public class BGMController : MonoBehaviour {
+        #region  Variables
+        [SerializeField]
+        private AudioClip[] bgmTracks;
+        [SerializeField]
+        private AudioMixer audioMixer;
+        [SerializeField]
+        private bool autoStart;
+        [SerializeField]
+        private bool isCyclicModeOn;
+        [SerializeField]
+        private float waitingTimeToRefreshInSeconds = 1f;
 
-            private AudioSource audioSource;
+        private AudioSource audioSource;
 
-            private bool isPlaying = false;
-            private int counter = 0;
-            private string channelName = "BGM";
-            #endregion
-
-
-            #region MonoBehavior functions
-            private void Start() {
-                audioSource = GetComponent<AudioSource>();
-
-                if( !audioSource )
-                    audioSource = gameObject.AddComponent<AudioSource>();
-
-                audioSource.outputAudioMixerGroup = audioMixer.FindMatchingGroups( channelName )[0];
-                audioSource.clip = bgmTracks[0];
-
-                if( autoStart ) {
-                    if( isCyclicModeOn ) {
-                        StartCoroutine( "CheckCyclicModePlaying" );
-
-                    } else {
-                        StartPlaying();
-                    }
-                }
-            }
-            #endregion
+        private bool isPlaying = false;
+        private int counter = 0;
+        private string channelName = "BGM";
+        #endregion
 
 
-            #region Public functions
-            public virtual void StartPlaying() {
-                if (!isPlaying) {
-                    if ( counter == bgmTracks.Length - 1) {
-                        counter = 0;
-                    }
+        #region MonoBehavior functions
+        private void Start() {
+            audioSource = GetComponent<AudioSource>();
 
-                    audioSource.clip = bgmTracks[counter];
-                    audioSource.Play();
+            if( !audioSource )
+                audioSource = gameObject.AddComponent<AudioSource>();
 
-                    counter++;
-                    isPlaying = true;
-                }
-            }
+            audioSource.outputAudioMixerGroup = audioMixer.FindMatchingGroups( channelName )[0];
+            audioSource.clip = bgmTracks[0];
+            audioSource.loop = isCyclicModeOn;
 
-            public virtual void StopPlaying() {
-                if (isPlaying) {
-                    audioSource.Stop();
-                    isPlaying = false;
-                }
-            }
+            if( autoStart )
+                audioSource.Play();
 
-            public void PlayNextSong() {
-                if( counter >= bgmTracks.Length
-                            ||  counter == bgmTracks.Length - 1 ) {
+        }
+        #endregion
+
+
+        #region Public functions
+        public void PlaySong( AudioClip _audioClip ) {
+            audioSource.clip = _audioClip;
+        }
+
+
+        /*
+        public virtual void StartPlaying() {
+            if( !isPlaying ) {
+                if( counter == bgmTracks.Length - 1 ) {
                     counter = 0;
                 }
 
@@ -88,39 +69,63 @@ namespace Jam15.Misc {
                 audioSource.Play();
 
                 counter++;
-            }
-
-            public void Pause() {
-                audioSource.Pause();
-            }
-
-            public void Resume() {
-                audioSource.UnPause();
-            }
-            #endregion
-
-
-            #region Private functions
-            private int GetCurrentIndex() {
-                if (counter == 0)
-                    return bgmTracks.Length - 1;
-                else
-                    return counter - 1;
-            }
-            #endregion
-
-
-            #region Coroutines
-            public IEnumerator CheckCyclicModePlaying() {
                 isPlaying = true;
-
-                while (isPlaying) {
-                    if( !audioSource.isPlaying ) 
-                        PlayNextSong();
-
-                    yield return new WaitForSecondsRealtime( waitingTimeToRefreshInSeconds );
-                }
             }
-            #endregion
         }
+
+        public virtual void StopPlaying() {
+            if( isPlaying ) {
+                audioSource.Stop();
+                isPlaying = false;
+            }
+        }
+
+        public void PlayNextSong() {
+            if( counter >= bgmTracks.Length
+                        || counter == bgmTracks.Length - 1 ) {
+                counter = 0;
+            }
+
+            audioSource.clip = bgmTracks[counter];
+            audioSource.Play();
+
+            counter++;
+        }
+
+        public void Pause() {
+            audioSource.Pause();
+        }
+
+        public void Resume() {
+            audioSource.UnPause();
+        }
+        */
+        #endregion
+
+
+        #region Private functions
+        /*
+        private int GetCurrentIndex() {
+            if( counter == 0 )
+                return bgmTracks.Length - 1;
+            else
+                return counter - 1;
+        }
+        #endregion
+
+
+        #region Coroutines
+        public IEnumerator CheckCyclicModePlaying() {
+            isPlaying = true;
+
+            while( isPlaying ) {
+                if( !audioSource.isPlaying )
+                    PlayNextSong();
+
+                yield return new WaitForSecondsRealtime( waitingTimeToRefreshInSeconds );
+            }
+        }
+        */
+        #endregion
     }
+}
